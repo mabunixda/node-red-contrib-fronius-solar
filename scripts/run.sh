160c135c7ea2fa518e4bc66480ab61f251a164e8
Martin Buchleitner <mabunixda@gmail.com>
2020-10-15 11:54:11 +0200

    Addons (#7)
    * adding additional queries to fronius api
    
    Signed-off-by: Martin Buchleitner <mabunixda@gmail.com>
    
    * audit fix
    
    Signed-off-by: Martin Buchleitner <mabunixda@gmail.com>
    
    * update deps
    
    Signed-off-by: Martin Buchleitner <mabunixda@gmail.com>

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
-docker run -u root --rm -it \
---network host \
--v "$PWD/:/src" \
-nodered/node-red-docker:slim-v10 \
-/src/scripts/docker-run.sh
+docker run \
+    -u root \
+    --rm -it \
+    --network host \
+    --entrypoint /src/scripts/docker-run.sh \
+    -v "$PWD/:/src" \
+    nodered/node-red:latest-12-minimal
