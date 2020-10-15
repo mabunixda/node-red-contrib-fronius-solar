0392584986800074cf411f06764b130f6680f289
Martin Buchleitner <mabunixda@gmail.com>
2020-10-15 11:44:50 +0200

    audit fix
    Signed-off-by: Martin Buchleitner <mabunixda@gmail.com>

--- scripts/run.sh
+++ scripts/run.sh
@@ -4,6 +4,6 @@ docker run \
     -u root \
     --rm -it \
     --network host \
+    --entrypoint /src/scripts/docker-run.sh \
     -v "$PWD/:/src" \
-    nodered/node-red:latest-12-minimal \
-    /src/scripts/docker-run.sh
+    nodered/node-red:latest-12-minimal
